6c150175910633d2e0099b7d762fdcb5f3f7fd15
Eizan <ivaneizan92@gmail.com>
2022-11-29 17:00:23 +0700

    fungsi insert dan validasi

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 const UserController = require('./controllers/UserController')
+const { body, validationResult } = require('express-validator');
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +21,32 @@ app.get('/', (req, res) => {
 
 app.get('/users', UserController.get);
 app.get('/users/:id', UserController.getdetail);
+app.post('/postusers', UserController.postInsert);
+app.post(
+    '/postusersvalidated', 
+    // password must be at least 4 chars long
+    body('name').isLength({ min: 4 }).withMessage('must be at least 4 chars longmust be in e-mail format'),
+    // username must be an email
+    body('email').isEmail().withMessage('username must be an email'),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 5 }).withMessage('must be at least 5 chars longmust be in e-mail format'),
+    (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        
+        User.create({
+            name: req.body.name ,
+            email : req.body.email,
+            password : req.body.password,
+            createdAt : new Date,
+            updatedAt : new Date,
+        }).then(user => res.json(user));
+    }
+);
 
 app.listen(port, () => {
-    console.log('Server is Running');
+    console.log(' Server is Running');
 });
