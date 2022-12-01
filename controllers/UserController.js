a71f906d3772db0b9645ac8a4a86dd44888632fc
Eizan <ivaneizan92@gmail.com>
2022-12-01 18:19:32 +0700

    upsert

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const { User } = require('../models');
 // INI UNTUK PAKAI RAW QUERY
 // const Sequelize = require('sequelize');
@@ -95,6 +96,83 @@ class UserController {
         }
     }
 
+    static async postUpdate(req, res) {
+        try{
+            const userUpdated = await User.update(
+                { name: 'test update' },
+                { where: { id: 4 } }
+              )
+            if(userUpdated){
+                return res.json({message:"User updated successfully",data:userUpdated})
+            }
+        }catch(errors){
+            console.log(errors);
+            res.status(400).json({
+                ok: false,
+                errors
+            });
+        }
+    };
+
+    static async postUpdateWithBody(req, res) {
+        try{
+            const atas = await updateOrCreate(User, {id: req.body.id ?? 0}, {name: req.body.name, email: req.body.email, password: req.body.password});
+            // .then(function(result) {
+            //     result.item;  // the model
+            //     result.created; // bool, if a new item was created.
+            // });
+            res.status(200).json({
+                message: 'Success',
+                created: atas.created,
+            });    
+        } catch (error) {
+            res.status(500).json({
+                message: 'Internal Server Error',
+                messageDetail: error,
+            })
+        }
+    };
+
+    static async postDinamicUpsert(req, res) {
+        try{
+            var a;
+            // console.log(req.body.tableName);
+            if (req.body.tableName === "User") {
+                a = User
+            }
+            const atas = await updateOrCreate(a, req.body.tableUnique, req.body.details);
+            res.status(200).json({
+                message: 'Success',
+                created: atas.created,
+            });    
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'Internal Server Error',
+                messageDetail: error,
+            })
+        }
+    };
+
+};
+
+function updateOrCreate (model, where, newItem) {
+    // First try to find the record
+    console.log(model);
+    return model
+    .findOne({where: where})
+    .then(function (foundItem) {
+        if (!foundItem) {
+            // Item not found, create a new one
+            return model
+                .create(newItem)
+                .then(function (item) { return  {item: item, created: true}; })
+        }
+         // Found an item, update it
+        return model
+            .update(newItem, {where: where})
+            .then(function (item) { return {item: item, created: false} });
+    });
 };
 
 module.exports = UserController;
